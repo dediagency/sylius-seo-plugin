e32f1ede6ef4a2298690459ac11267c5a4356eca
Fred <frederic@dedi-agency.com>
2024-06-06 11:04:31 +0200

    feat: sylius 1.13 compatibility

---
+++ tests/Application/config/sylius/1.13/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
+];
